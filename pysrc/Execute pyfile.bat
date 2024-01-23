55fe406de023ac251db90d4109ff3a8d546a5986
conradoguimaraes <up202102679@up.pt>
2024-01-23 08:23:49 +0000

    [fix] init

---
+++ pysrc/Execute pyfile.bat
@@ -0,0 +1,1 @@
+powershell.exe -ExecutionPolicy ByPass -noexit -command "& 'C:\Users\Admin\anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\Admin\anaconda3'; conda activate main; cd 'C:\Users\Admin\Documents\Github\Path-Following\pysrc'; cls; python test.py
